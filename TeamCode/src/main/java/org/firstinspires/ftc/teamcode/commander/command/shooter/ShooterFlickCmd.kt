81efffaa2d054f149bc2b97894724148d59e239f
Manini30 <leonel.chavez@secundariatec.edu.mx>
2021-06-22 12:38:47 -0600

    Fixed flicker as manini likes it lol

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commander/command/shooter/ShooterFlickCmd.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commander/command/shooter/ShooterFlickCmd.kt
@@ -3,50 +3,25 @@ package org.firstinspires.ftc.teamcode.commander.command.shooter
 import com.github.serivesmejia.deltacommander.DeltaCommand
 import com.noahbres.jotai.StateMachine
 import com.noahbres.jotai.StateMachineBuilder
+import com.qualcomm.robotcore.util.ElapsedTime
 import org.firstinspires.ftc.teamcode.Constants
 import org.firstinspires.ftc.teamcode.commander.subsystem.ShooterFlickerSubsystem
 
-class ShooterFlickCmd(val isInfinite: Boolean = true) : DeltaCommand() {
+class ShooterFlickCmd() : DeltaCommand() {
 
     val shooterSub = require<ShooterFlickerSubsystem>()
 
-    enum class State { IN, OUT, FINISHED }
-
-    private lateinit var stateMachine: StateMachine<State>
+    val timer = ElapsedTime()
 
     override fun init() {
-        stateMachine = StateMachineBuilder<State>()
-            .state(State.IN)
-            .transitionTimed(0.4)
-            .loop {
-                shooterSub.flickerServo.position = Constants.flickerInPos
-            }
-
-            .state(State.OUT)
-            .transitionTimed(0.4)
-            .loop {
-                shooterSub.flickerServo.position = Constants.flickerOutPos
-            }
-
-            .state(State.FINISHED)
-            .transitionTimed(0.2)
-
-            .exit(State.IN)
-
-            .build()
-
-        stateMachine.looping = true
-        stateMachine.start()
+        timer.reset()
     }
 
     override fun run() {
-        stateMachine.update()
-
-        if(!isInfinite && stateMachine.getState() == State.FINISHED)
-            requestFinish()
+        shooterSub.flickerServo.position = Constants.flickerInPos
     }
 
-    override fun endCondition() = stateMachine.getState() == State.FINISHED
+    //override fun endCondition() = timer.seconds() >= 0.5
 
 }
 
